9b4e6bc674ecf34cd5184ae2c2ee389172dfffe1
Justin Searles <jsearles0619@gmail.com>
2012-04-19 16:35:14 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ventureboard::Application.config.secret_token = '2d49ffd48e0d9a302dc9f39e505a02a86da75ee90960c800e4108479d0268c101ad249b0ca02033906fb0bbecb0ffeaddea332d1857d6dddc842fd906b24340d'
