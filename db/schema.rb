4eabc2958f781655569584f17125f574ae144db5
Scott Block <scott@scottblock.com>
2012-04-20 00:45:31 -0400

    fixed signup form

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(:version => 20120419224607) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
   create_table "skills", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
@@ -45,11 +55,6 @@ ActiveRecord::Schema.define(:version => 20120419224607) do
 
   create_table "types", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "user_sessions", :force => true do |t|
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
