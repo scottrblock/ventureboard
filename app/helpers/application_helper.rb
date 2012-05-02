6d585a9271b0550e18f904068ea89508b71638a6
Justin Searles <jsearles0619@gmail.com>
2012-05-02 16:22:47 -0400

    Got rid of the JS forms, made it a multiple select for majors and minors.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,2 @@
 module ApplicationHelper
-  def link_to_add_fields(name, f, association)
-    debugger
-    object = f.object.send(association).klass.new
-    id = 1 
-    fields = fields_for(association, object, child_index: id) do |builder|
-      render(association.to_s.singularize + "_fields", f: builder )
-    end
-    link_to(name, '#', class: "add-fields field-add-" + association.to_s.singularize, data: {id: id, fields: fields.gsub("\n", "")})  
-  end
 end
